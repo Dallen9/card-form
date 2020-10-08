5e266859ed7392ded25f8144832d761d4e745e27
Donovan <allendonovan@gmail.com>
2020-10-07 20:26:13 -0700

    fixed mobile saved image view

--- src/Components/Success.js
+++ src/Components/Success.js
@@ -16,13 +16,14 @@ const Success = ({onSubmit, name, notValid}) => {
     const downloadImage = () => {
       let container = document.getElementById("FrontSide"); 
 
-      html2canvas(container, {scrollX: 0, scrollY: 0}).then(function (canvas) {
+      html2canvas(container, {scrollY: -window.scrollY}).then(function (canvas) {
 
           let link = document.createElement("a");
           document.body.appendChild(link);
           link.download = `${name}'s-card.png`;
           link.href = canvas.toDataURL();
           link.click();
+          window.open(link);
 
           document.body.removeChild(link);
 
